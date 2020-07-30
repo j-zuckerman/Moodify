bcefea30c9d019183468aa6334267964381b4828
James Zuckerman <jzuckerman4@gmail.com>
2020-07-30 00:47:37 -0700

    Updates

---
+++ precache-manifest.909ad306eae442e1ab35eea8a69deec7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b05b937d726983140dfe5e8e3bbe229c",
+    "url": "/Moodify/index.html"
+  },
+  {
+    "revision": "db2931a600585223f955",
+    "url": "/Moodify/static/css/main.e6986258.chunk.css"
+  },
+  {
+    "revision": "d6893bcb7e8f210b1c7a",
+    "url": "/Moodify/static/js/2.d35218fd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Moodify/static/js/2.d35218fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db2931a600585223f955",
+    "url": "/Moodify/static/js/main.0eaf87f5.chunk.js"
+  },
+  {
+    "revision": "525839d7dc8670489595",
+    "url": "/Moodify/static/js/runtime-main.b2c65d49.js"
+  }
+]);
